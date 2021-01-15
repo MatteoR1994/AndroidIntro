afb1ddac4a73280c84e9eec7743566a32daa6037
Matteo Rinaldi <matteo.rinaldi1893@gmail.com>
2021-01-15 12:36:35 +0100

    Modifiche per gestione ID elementi

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -5,18 +5,19 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val input = findViewById<EditText>(R.id.editTextTextPersonName)
-        val output = findViewById<TextView>(R.id.testoSaluto)
-        val button = findViewById<Button>(R.id.actionButton)
+
+       // val input = findViewById<EditText>(R.id.editTextTextPersonName)
+       // val output = findViewById<TextView>(R.id.testoSaluto)
+      //  val button = findViewById<Button>(R.id.actionButton)
 //        button.setOnClickListener { Toast.makeText(this, // Toast = messaggio "pop-up" mostrato sopra all'activity
 //        "ciao bello", Toast.LENGTH_LONG).show() }
-        button.setOnClickListener {
-            output.text = "Ciao ${input.text}!"
+        actionButton.setOnClickListener {
+            testoSaluto.text = "Ciao ${editTextTextPersonName.text}!"
         }
     }
 }
